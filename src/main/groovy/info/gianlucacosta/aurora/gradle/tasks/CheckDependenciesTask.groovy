09f3cdba2844d2d35da61e9b5d8845ef4a68485d
Gianluca Costa <gianluca@gianlucacosta.info>
2024-07-12 12:52:18 +0200

    Update code

--- src/main/groovy/info/gianlucacosta/aurora/gradle/tasks/CheckDependenciesTask.groovy
+++ src/main/groovy/info/gianlucacosta/aurora/gradle/tasks/CheckDependenciesTask.groovy
@@ -5,16 +5,18 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
 class CheckDependenciesTask extends DefaultTask {
+
     @TaskAction
     def checkDependencies() {
         project.configurations.asList().each { configuration ->
             configuration.getAllDependencies().each { dependency ->
-                if (dependency.hasProperty("version")) {
-                    if (dependency.version != null && dependency.version.endsWith("-SNAPSHOT")) {
+                if (dependency.hasProperty('version')) {
+                    if (dependency.version != null && dependency.version.endsWith('-SNAPSHOT')) {
                         throw new AuroraException("You should not depend on a SNAPSHOT library: '${dependency.group}:${dependency.name}:${dependency.version}'")
                     }
                 }
             }
         }
     }
+
 }
